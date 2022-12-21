0802327bc7f82a41f7169b1b4e4d624b251eb718
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2022-12-21 18:53:20 +0200

    install lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,9 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+
+console.log(SimpleLightbox)
+
+
 console.log(galleryItems);
