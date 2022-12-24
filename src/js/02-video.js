aec4df80eabaaca93dc1bf1e5bf93df63758fccf
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2022-12-24 19:32:25 +0200

    fix task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,4 +10,4 @@ function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));n
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
